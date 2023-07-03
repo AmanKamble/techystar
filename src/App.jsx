22fad9ebe3aa1bcd859a52dd0f59312ae07ce305
AmanKamble <amankamble88@gmail.com>
2023-07-03 14:22:53 +0530

    service page
    i have created the services page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import "./Styles/App.css"
 import Header from './Components/Header';
 import Home from './Components/Home';
-import Footer from './Components/Footer';
 import Contact from './Components/Contact';
+import Services from './Components/Services';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/services' element={<Services />} />
       </Routes>
       <Footer />
     </Router>
